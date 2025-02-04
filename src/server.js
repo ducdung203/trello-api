6f8383dd13dee4976ca6abfe11b4bf457b69bffa
ducdung203 <anhemvuive79@gmail.com>
2025-02-04 16:37:41 +0700

    Init Code Base

--- src/server.js
+++ src/server.js
@@ -1,16 +1,33 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
 
-// Middleware để xử lý JSON
-app.use(express.json());
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
-// Route đơn giản
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
 app.get('/', (req, res) => {
-    res.send('Hello, Express!');
-});
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
+})
 
-// Lắng nghe cổng
-app.listen(PORT, () => {
-    console.log(`Server đang chạy tại http://localhost:${PORT}`);
-});
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at http://${ hostname }:${ port }/`)
+})
